725cca8987ffa3a972119d6c9dc8bbb622ddd87e
So Mei Amy Lam <lamsomeiamy@gmail.com>
2025-01-27 13:15:04 -0800

    Added functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,17 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const item = action.payload;
+      item.quantity = 1;
+      state.items.push(item);
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const item = state.items.find((item) => item.name === name);
+      item.quantity = quantity;
     },
   },
 });
